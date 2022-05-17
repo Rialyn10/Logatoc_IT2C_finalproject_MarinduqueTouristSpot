3a6c899c71658180bb82ac991461e3c97d63a4ad
LogatocRialyn <logatocrialyn>
2022-05-17 00:31:09 -1200

    initial files

---
+++ info.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>resort</title>
+</head>
+<body class="htips">
+  <header>
+    <div class="nav">
+      <ul class="nav-ul">
+              <li class="nav-li"><a href="home.php" >Home</a></li>
+              <li class="nav-li"><a href="gallery.php" >Photos</a></li>
+            
+              <li class="nav-li"><a href="info.php" >Info</a></li>
+              <li class="nav-li"><a href="history.php" >History</a></li>
+              <li class="nav-li"><a href="reservation.php" >Book</a></li>
+
+          </ul>
+      </div>
+</header> 
+<body>
+</header> 
+<link href="style.css" rel="stylesheet">
+<body><br><br>
+<div class="amn"> 
+  <style="border-radius: 18px;">
+
+    <center> <h1>Marinduque Tourist Spot Info </h1></center>
+    <p> <ul class="amn-ul">
+              <p class="nav-li"><a href="maniwaya.php" > Maniwaya Island</a></p>
+              <p class="nav-li"><a href="poctoy.php" >Poctoy White Beach</a></p>
+            
+              <p class="nav-li"><a href="bagumbayan.php" > Bagumbayan Cave </a></p>
+              <p class="nav-li"><a href="malbog.php" > Malbog Sulfur Spring </a></p>
+              <p class="nav-li"><a href="mongpong.php" > Mongpong Island </a></p>
+              <p class="nav-li"><a href="tresreyes.php" > Tres Reyes Island </a></p>
+              <p class="nav-li"><a href="paadjao.php" > Paadjao Falls </a></p>
+              <p class="nav-li"><a href="bathala.php" > Bathala Cave </a></p>
+              <p class="nav-li"><a href="malindig.php" > Mount Malindig </a></p>
+              <p class="nav-li"><a href="kawakawa.php" > Kawa kawa Falls </a></p>
+          </p>
+      
+      </div>
+    
+    </body>
+    <style>
+         body{
+     background-color: grey;
+     background-size: cover;
+     background-repeat: no-repeat;
+    }
+    
+    h1{
+      color: #232d60;
+    }
+    p{
+      color:whitesmoke;
+    }
+    
+
+    }
+    </style>
+</html>
